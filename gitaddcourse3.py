194074fd60225e49640f167c35a5edc5c0defbd6
meetpatel1812 <71310616+meetpatel1812@users.noreply.github.com>
2024-10-18 01:37:35 -0400

    Update gitaddcourse3.py

--- gitaddcourse3.py
+++ gitaddcourse3.py
@@ -29,8 +29,7 @@ stream_options = [
 semester_options = ["Fall", "Winter", "Summer"]
 
 # GitHub repository details
-# github_token = st.secrets["GITHUB_TOKEN"]  # Store token securely using Streamlit secrets
-github_token="GITHUB_TOKEN"
+github_token = st.secrets["GITHUB_TOKEN"]  # Store token securely using Streamlit secrets
 repo_name = "meetpatel1812/mcmaster_course_outline_app"
 file_path = "pdf_data.py"  # Update this with the correct path in your repo
 
